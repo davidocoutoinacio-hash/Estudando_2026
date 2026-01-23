018bdeff173f2db87fb66dad896d954721ca3c06
DAVI I.C <davidocoutoinacio@gmail.com>
2026-01-22 22:09:57 -0300

    Add initial App.java with basic main method
    Introduces App.java containing a main method that prints 'Hello, World!' and attempts to read an integer from input. Basic error handling is included to display any exceptions encountered.

---
+++ conder/com/src/App.java
@@ -0,0 +1,35 @@
+package conder.com.src;
+
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        try {
+
+               System.out.println("Hello, World!");
+               scanner.nextInt(1);
+            
+        } catch (Exception e) {
+
+            System.out.println("An error occurred: " + e.getMessage() );
+           
+          
+    }
+
+
+        }
+
+    }
+
+
+     
+
+    
+    
+
