ba711fd178dc2dfe2be48e7ef1cb7cd83d3eeac5
DAVI I.C <davidocoutoinacio@gmail.com>
2026-01-23 21:18:39 -0300

    Add nested loops for minutes and seconds output
    Introduced two additional nested loops to iterate over minutes (j) and seconds (k), printing their values alongside hours (i) with a 1-second delay between each print. This enhances the time simulation by including minutes and seconds.

--- Conteudos/Paralelo/Testando_For/UsoHoras.java
+++ Conteudos/Paralelo/Testando_For/UsoHoras.java
@@ -6,23 +6,32 @@ package Estudando_2026.Conteudos.Paralelo.Testando_For;
                 public static void main(String[] args) {
 
 
-                    for (int i = 1; i < 100; i++) {
+                    for (int i = 1; i < 99; i++) {
+                        for (int j = 100; j < 199; j++) {
+                            for (int k = 200; k < 299; k++) {
 
 
                         try {
-                            Thread.sleep(1000);
+                                Thread.sleep(1000);
                                System.out.println("Hora número: " + i);
+                                Thread.sleep(1000);
+                                System.out.println("Minuto número: " + j);
+                                Thread.sleep(1000);
+                                System.out.println("Segundo número: " + k);
+
+
+
                         } catch (Exception e) {
 
+                        }
+
+                    }
 
 
                         }
 
+                    }
 
-                        }
-
-                        
-                    }    
-                
-                }
+                    }
+                }  
             
