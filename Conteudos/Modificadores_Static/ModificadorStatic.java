2170453cb77fba232e67620e1c408d238ef2bcbc
DAVI I.C <davidocoutoinacio@gmail.com>
2026-02-08 23:53:13 -0300

    Update ModificadorStatic.java

--- Conteudos/Modificadores_Static/ModificadorStatic.java
+++ Conteudos/Modificadores_Static/ModificadorStatic.java
@@ -2,21 +2,36 @@ public class ModificadorStatic {
         
 
     public static void main(String[] args) {
-        Conta contaMaria = new Conta();  
-        Conta contaJoao = new Conta();  
-       
 
-        contaMaria.depositarContaConjunta(1000);
-        contaMaria.depositarConta(0);
-        contaJoao.depositarConta(100);
 
-    
+    // Uso de Static
+    // 1. Quando não precisamos de objeto
+    // 2. Quando queremos criar constantes, como o valor de pi por exemplo
+    // 3. Classes uilitarias que não dependem de objetos
 
-        System.out.println(Conta.saldoContaConjunta);
+        //    Conta contaMaria = new Conta();  
+        //    Conta contaJoao = new Conta();  
+        //    Conta.depositarContaConjunta(1000);
+        //    contaMaria.depositarConta(0);
+        //    contaJoao.depositarConta(100);
+        //    double resultadoFinal = Conta.saldoContaConjunta - Conta.TAXA_SERVICO;
+        //    System.out.println("Saldo: " + Conta.saldoContaConjunta);
+        //    System.out.println("Taxa: " + Conta.TAXA_SERVICO);
+        //System.out.println("Resultado final: " + resultadoFinal);
+        //  System.out.println(contaJoao.saldo);
+        // System.out.println(contaMaria.saldo);
+
+        Conta contaJoao = new Conta();
+        contaJoao.depositarConta(1.555);
         System.out.println(contaJoao.saldo);
-        System.out.println(contaMaria.saldo);
-       
+        System.out.println(Math.round(contaJoao.saldo));
+        //Metodo Math é nativo da linguagem java, ele tem varias operações matematicas já instanciadas para o uso 
+        // fazendo com que a necessidade de instanciar operações matematicas nos codigos seja menos necessaria 
+        // floor no metodo Math serve para arredondar o numero para baixo
+        // ceil serve para arredondar para cima
+        // round serve para arredondar altomaticamente para o valor mais proximo.
 
+       
 
 
 
